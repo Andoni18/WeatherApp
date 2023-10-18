7b937c8a95138a521074521f076cc9d30dfc7e48
Andoni <andoni.fernandezlo@elorrieta-errkamari.com>
2023-10-18 08:54:33 +0200

    Agregar archivos de proyecto.

---
+++ Config.cs
@@ -0,0 +1,7 @@
+﻿namespace WeatherApp
+{
+    public class Config
+    {
+        public static string GeoApifyKey = "f1ce03e4d083426481bf63faf95866bc";
+    }
+}
---
+++ Endpoints.cs
@@ -0,0 +1,16 @@
+﻿namespace WeatherApp
+{
+    public class Endpoints
+    {
+        public static string GetGeoApifyKeyEndpoint(string cityname)
+        {
+            return $"https://api.geoapify.com/v1/geocode/search?text={cityname}&format=json&apiKey={Config.GeoApifyKey}";
+            //return $"https://api.geoapify.com/v1/geocode/search?text=bilbao&format=json&apiKey=YOUR_API_KEY"
+        }
+        public static string GetWeatherEndpoint(float lat, float lon)
+        {
+            var url = $"https://api.open-meteo.com/v1/forecast?latitude={lat}&longitude={lon}&current=temperature_2m,weathercode,windspeed_10m,winddirection_10m&hourly=temperature_2m,weathercode&daily=apparent_temperature_max,apparent_temperature_min&timezone=Europe%2FBerlin";
+            return url ;
+        }
+    }
+}
